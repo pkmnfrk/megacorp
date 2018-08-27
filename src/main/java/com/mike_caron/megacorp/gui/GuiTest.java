4c38dcfdfcc25c2b10ac46eddc5fde86ccb201ac
Mike Caron <caron.mike@gmail.com>
2018-08-27 18:20:24 -0400

    first implementation of the list

--- src/main/java/com/mike_caron/megacorp/gui/GuiTest.java
+++ src/main/java/com/mike_caron/megacorp/gui/GuiTest.java
@@ -6,15 +6,18 @@ import com.mike_caron.megacorp.gui.control.GuiContainerBase;
 import com.mike_caron.megacorp.gui.control.GuiList;
 import net.minecraft.util.ResourceLocation;
 
+import java.awt.*;
+
 public class GuiTest
     extends GuiContainerBase
+    implements GuiList.Producer
 {
     public static final int WIDTH = 176;
     public static final int HEIGHT = 166;
 
     private static final ResourceLocation background = new ResourceLocation(MegaCorpMod.modId, "textures/gui/uplink.png");
 
-    private GuiList list = new GuiList(10, 20, WIDTH - 20, 60);
+    private GuiList list = new GuiList(10, 20, WIDTH - 20, 60, this);
 
     private boolean goingLeft = false;
     private boolean goingUp = false;
@@ -49,5 +52,42 @@ public class GuiTest
     }
 
 
+    @Override
+    public int getNumItems()
+    {
+        return 3;
+    }
 
+    @Override
+    public int getItemHeight()
+    {
+        return 20;
+    }
+
+    @Override
+    public GuiList.ListItem getItem(int i)
+    {
+        return new TestListItem(Integer.toString(i));
+    }
+
+    class TestListItem
+        implements GuiList.ListItem
+    {
+        String string;
+
+        public TestListItem(String string)
+        {
+            this.string = string;
+        }
+
+        @Override
+        public void draw(int width, int height)
+        {
+            GuiUtil.setGLColor(Color.RED);
+            GuiUtil.bindTexture(GuiUtil.MISC_RESOURCES);
+            GuiUtil.draw3x3Stretched(0, 0, width, height, 16, 16);
+
+            fontRenderer.drawString(string, 1, 1, Color.WHITE.getRGB());
+        }
+    }
 }
--- src/main/java/com/mike_caron/megacorp/gui/control/GuiList.java
+++ src/main/java/com/mike_caron/megacorp/gui/control/GuiList.java
@@ -1,16 +1,20 @@
 package com.mike_caron.megacorp.gui.control;
 
 import com.mike_caron.megacorp.gui.GuiUtil;
+import org.lwjgl.opengl.GL11;
 
 import java.awt.*;
 
 public class GuiList
     extends GuiClippedSized
 {
+    Producer producer;
 
-    public GuiList(int x, int y, int width, int height)
+    public GuiList(int x, int y, int width, int height, Producer producer)
     {
         super(x, y, width, height);
+
+        this.producer = producer;
     }
 
     @Override
@@ -19,7 +23,54 @@ public class GuiList
         // TODO: implement
         GuiUtil.setGLColor(Color.WHITE);
         GuiUtil.bindTexture(GuiUtil.MISC_RESOURCES);
-        GuiUtil.draw3x3Stretched(0, 0, this.width, this.height, 16, 16);
+        //TODO: this is not correct
+        GuiUtil.draw3x3Stretched(scrollX, scrollY, this.width, this.height, 16, 16);
 
+        if(this.producer != null)
+        {
+            int itemHeight = this.producer.getItemHeight();
+            int numItems = maxVisibleItems();
+
+            int numSkipped = scrollY / itemHeight;
+
+            GL11.glPushMatrix();
+            GL11.glTranslatef(1, 1, 0);
+            for(int i = 0; i < numItems && i + numSkipped < this.producer.getNumItems(); i++)
+            {
+                ListItem item = this.producer.getItem(i + numSkipped);
+
+                item.draw(this.width - 2, itemHeight);
+                GL11.glTranslatef(0, itemHeight, 0);
+
+            }
+            GL11.glPopMatrix();
+
+        }
+    }
+
+    private int maxVisibleItems()
+    {
+        if(this.producer == null) return 0;
+
+        return 2 + this.height / this.producer.getItemHeight();
+    }
+
+    private int maxScrollHeight()
+    {
+        if(this.producer == null) return 0;
+        return Math.max(0, this.producer.getItemHeight() * this.producer.getNumItems() - (this.height - 2));
+    }
+
+    public interface Producer
+    {
+        int getNumItems();
+        int getItemHeight();
+        ListItem getItem(int i);
+    }
+
+    public interface ListItem
+    {
+        //void draw(int y);
+        void draw(int width, int height);
     }
 }

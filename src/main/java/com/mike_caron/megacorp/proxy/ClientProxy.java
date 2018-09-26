c4cabe27a833e2a200041e8c955aad14e1ae5fca
Mike Caron <caron.mike@gmail.com>
2018-09-25 22:45:28 -0400

    tidy up the oredict stuff, especially since there are APIs to access the data I was poking in!

--- src/main/java/com/mike_caron/megacorp/proxy/ClientProxy.java
+++ src/main/java/com/mike_caron/megacorp/proxy/ClientProxy.java
@@ -1,12 +1,10 @@
 package com.mike_caron.megacorp.proxy;
 
-import com.mike_caron.megacorp.MegaCorpMod;
 import com.mike_caron.megacorp.block.ModBlocks;
 import com.mike_caron.megacorp.client.models.ModelBottle;
 import com.mike_caron.megacorp.item.ModItems;
+import com.mike_caron.megacorp.util.OreDictUtil;
 import net.minecraft.client.renderer.block.model.ModelBakery;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.NonNullList;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoaderRegistry;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
@@ -14,10 +12,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.oredict.OreDictionary;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
 import java.util.List;
 
 @Mod.EventBusSubscriber(Side.CLIENT)
@@ -53,42 +48,11 @@ public class ClientProxy
 
         if(event.getFlags().isAdvanced())
         {
-            try
+            List<String> dicts = OreDictUtil.getDictsForItem(event.getItemStack());
+
+            for (String dict : dicts)
             {
-                Field f = OreDictionary.class.getDeclaredField("idToStack");
-                f.setAccessible(true);
-                List<NonNullList<ItemStack>> oreRegistry = (List<NonNullList<ItemStack>>)f.get(null);
-                List<Integer> relevantIds = new ArrayList<>();
-
-                for(int oreId = 0; oreId < oreRegistry.size(); oreId ++)
-                {
-                    NonNullList<ItemStack> ores = oreRegistry.get(oreId);
-                    for(ItemStack is : ores)
-                    {
-                        if(is.isItemEqual(event.getItemStack()))
-                        {
-                            relevantIds.add(oreId);
-                            break;
-                        }
-                    }
-                }
-
-                if(relevantIds.size() > 0)
-                {
-                    f = OreDictionary.class.getDeclaredField("idToName");
-                    f.setAccessible(true);
-                    List<String> idToName = (List<String>)f.get(null);
-
-                    for (int id : relevantIds)
-                    {
-                        event.getToolTip().add("OD: " + idToName.get(id));
-                    }
-                }
-
-            }
-            catch (Exception ex)
-            {
-                MegaCorpMod.logger.error("Error getting ore dict data", ex);
+                event.getToolTip().add("OD: " + dict);
             }
         }
     }

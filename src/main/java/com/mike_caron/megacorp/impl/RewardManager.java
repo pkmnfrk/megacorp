00234a245d6c9cbe43a9b81e84554efe77f47512
Mike Caron <caron.mike@gmail.com>
2018-10-23 01:49:45 -0400

    implement the reward blacklist too (Closes #9)

--- src/main/java/com/mike_caron/megacorp/impl/RewardManager.java
+++ src/main/java/com/mike_caron/megacorp/impl/RewardManager.java
@@ -5,6 +5,7 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.mike_caron.megacorp.MegaCorpMod;
+import com.mike_caron.megacorp.ModConfig;
 import com.mike_caron.megacorp.api.IReward;
 import com.mike_caron.megacorp.api.IRewardFactory;
 import net.minecraft.client.Minecraft;
@@ -19,6 +20,7 @@ import javax.annotation.Nullable;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -39,83 +41,107 @@ public class RewardManager
 
         JsonParser parser = new JsonParser();
 
-        CraftingHelper.findFiles(Loader.instance().getReversedModObjectList().get(MegaCorpMod.instance), "assets/" + MegaCorpMod.modId + "/rewards", null, (root, url) ->
+        CraftingHelper.findFiles(Loader.instance().getReversedModObjectList().get(MegaCorpMod.instance), "assets/" + MegaCorpMod.modId + "/rewards", null, (root, url) -> loadRewards(parser, url, true)
+        , false, true);
+
+    }
+
+    protected Boolean loadRewards(JsonParser parser, Path url, boolean applyBlacklists)
+    {
+        String extension = FilenameUtils.getExtension(url.toString());
+
+        if("json".equals(extension))
         {
-            String extension = FilenameUtils.getExtension(url.toString());
-
-            if("json".equals(extension))
+            JsonArray json;
+            try (BufferedReader stream = Files.newBufferedReader(url))
             {
-                JsonArray json;
-                try (BufferedReader stream = Files.newBufferedReader(url))
-                {
-                    json = parser.parse(stream).getAsJsonArray();
-                }
-                catch(RuntimeException | IOException ex)
-                {
-                    MegaCorpMod.logger.error("Encountered error while reading " + url, ex);
-                    return true;
-                }
-
-                /*
-                String mod = json.get("mod").getAsString();
-
-                if(!Loader.isModLoaded(mod))
-                {
-                    MegaCorpMod.logger.info("Skipping " + mod + " quests, because it's not loaded");
-                }
-                */
-
-                for(JsonElement obj : json)
-                {
-                    try
-                    {
-                        JsonObject reward = (JsonObject)obj;
-
-                        String id = reward.get("id").getAsString();
-                        String factory = "com.mike_caron.megacorp.reward.GenericReward$Factory";
-
-                        if(reward.has("class"))
-                        {
-                            factory = reward.get("class").getAsString();
-                        }
-
-                        Class factoryClass = Class.forName(factory);
-
-                        IRewardFactory factoryInstance = (IRewardFactory)factoryClass.newInstance();
-
-                        IReward instance = factoryInstance.createReward(id, reward);
-
-                        rewards.put(id, instance);
-
-                        MegaCorpMod.logger.debug("Loaded reward " + id);
-                    }
-                    catch (Exception ex)
-                    {
-                        MegaCorpMod.logger.error("Error loading quests from " + url, ex);
-                    }
-                }
+                json = parser.parse(stream).getAsJsonArray();
             }
-            else if("lang".equals(extension))
+            catch(RuntimeException | IOException ex)
             {
-                String locale = FilenameUtils.getBaseName(url.toString()).toLowerCase();
-                if(!localizations.containsKey(locale))
-                {
-                    localizations.put(locale, new HashMap<>());
-                }
+                MegaCorpMod.logger.error("Encountered error while reading " + url, ex);
+                return true;
+            }
 
+            /*
+            String mod = json.get("mod").getAsString();
+
+            if(!Loader.isModLoaded(mod))
+            {
+                MegaCorpMod.logger.info("Skipping " + mod + " quests, because it's not loaded");
+            }
+            */
+
+            for(JsonElement obj : json)
+            {
                 try
                 {
-                    List<String> lines = Files.readAllLines(url);
-                    loadLocalization(locale, lines);
+                    JsonObject reward = (JsonObject)obj;
+
+                    String id = reward.get("id").getAsString();
+
+                    if(applyBlacklists)
+                    {
+                        boolean abort = false;
+
+                        for(int i = 0; i < ModConfig.rewardBlacklist.length; i++)
+                        {
+                            if(ModConfig.rewardBlacklist[i].equals(id))
+                            {
+                                abort = true;
+                                break;
+                            }
+                        }
+
+                        if(abort)
+                        {
+                            MegaCorpMod.logger.debug("Skipping reward " + id + " because it is on the blacklist");
+                            continue;
+                        }
+                    }
+
+                    String factory = "com.mike_caron.megacorp.reward.GenericReward$Factory";
+
+                    if(reward.has("class"))
+                    {
+                        factory = reward.get("class").getAsString();
+                    }
+
+                    Class factoryClass = Class.forName(factory);
+
+                    IRewardFactory factoryInstance = (IRewardFactory)factoryClass.newInstance();
+
+                    IReward instance = factoryInstance.createReward(id, reward);
+
+                    rewards.put(id, instance);
+
+                    MegaCorpMod.logger.debug("Loaded reward " + id);
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
-                    MegaCorpMod.logger.error("Error loading localization from " + url, ex);
+                    MegaCorpMod.logger.error("Error loading quests from " + url, ex);
                 }
             }
-            return true;
-        }, false, true);
+        }
+        else if("lang".equals(extension))
+        {
+            String locale = FilenameUtils.getBaseName(url.toString()).toLowerCase();
+            if(!localizations.containsKey(locale))
+            {
+                localizations.put(locale, new HashMap<>());
+            }
 
+            try
+            {
+                List<String> lines = Files.readAllLines(url);
+                loadLocalization(locale, lines);
+            }
+            catch(Exception ex)
+            {
+                MegaCorpMod.logger.error("Error loading localization from " + url, ex);
+            }
+        }
+        return true;
     }
 
     private void loadLocalization(@Nonnull String locale, @Nonnull List<String> lines)

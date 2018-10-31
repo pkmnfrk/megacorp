7257ad70d23479b8dc4bcaf4d5838f5083545592
Mike Caron <caron.mike@gmail.com>
2018-10-31 18:41:47 -0400

    fix the script url, and fix the permissions

--- publish_build.sh
+++ publish_build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 TZ='America/Toronto'
-NEWURL=https://${ARTIFACTS_BUCKET}/${TRAVIS_REPO_SLUG}/${TRAVIS_BUILD_NUMBER}/${TRAVIS_JOB_NUMBER}/`ls build/libs/*.jar`
+NEWURL=https://s3.amazonaws.com/${ARTIFACTS_BUCKET}/${TRAVIS_REPO_SLUG}/${TRAVIS_BUILD_NUMBER}/${TRAVIS_JOB_NUMBER}/`ls build/libs/*.jar`
 TEXT="Build #${TRAVIS_BUILD_NUMBER} - `date`"
 
 LINE="[${TEXT}](${NEWURL}) - [Commit](https://github.com/${TRAVIS_REPO_SLUG}/commit/${TRAVIS_COMMIT})"

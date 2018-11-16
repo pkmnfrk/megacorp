6b543b27b43a4927bbccd6ffc6fbdaffa899cf99
Mike Caron <caron.mike@gmail.com>
2018-11-15 20:22:08 -0500

    update some dependencies and the source mapping

--- src/main/java/com/mike_caron/megacorp/integrations/MainCompatHandler.java
+++ src/main/java/com/mike_caron/megacorp/integrations/MainCompatHandler.java
@@ -11,6 +11,7 @@ public class MainCompatHandler
         registerWaila();
         registerTConstruct();
         //registerEnderio();
+        registerGameStages();
     }
 
     public static void registerAllInit()
